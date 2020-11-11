6e8ceb01ba9e76b794f00e7d122c4aae0465c87c
Alejandro del Rio <adelriosantiago@gmail.com>
2020-11-11 02:00:55 -0600

    -Add script to build logView html -Logview now shows real data

--- buildHtml.js
+++ buildHtml.js
@@ -2,6 +2,6 @@ const fs = require("fs")
 
 let text = fs.readFileSync(`${__dirname}/html-dev/logView.html`, "utf8")
 text = text.replace(/\$/gm, "\\$").replace(/\`/gm, "\\`")
-fs.writeFileSync("logView.js", `const logView = \`${text}\`; module.exports = logView;`)
+fs.writeFileSync("./logView.js", `const logView = \`${text}\`; module.exports = logView;`, { encoding: "utf8", flag: "w" })
 
 console.log("Done building logView.js")
